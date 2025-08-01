1bf26c6240ce647fc852c68286c89decfa127042
PDKhan <kkzoro1@gmail.com>
2025-08-01 16:20:24 -0500

    20. Valid Parentheses

---
+++ valid_parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        std::stack<char> stack;
+
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == ')'){
+                if(stack.empty() || stack.top() != '(')
+                    return false;
+                stack.pop();
+            }else if(s[i] == '}'){
+                if(stack.empty() || stack.top() != '{')
+                    return false;
+                stack.pop();
+            }else if(s[i] == ']'){
+                if(stack.empty() || stack.top() != '[')
+                    return false;
+                stack.pop();
+            }else
+                stack.push(s[i]);
+        }
+
+        return stack.empty();
+    }
+};
